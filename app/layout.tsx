e03645b85974fae4438362da933d092fbcc7a040
salomontillaUni <salomon.montilla.l@uniautonoma.edu.co>
2026-03-07 23:17:10 -0500

    cambios en el layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,9 +25,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex-1 overflow-y-auto relative flex flex-col min-w-0 pt-16 lg:pt-0`}
       >
-        {children}
+        <div className="flex-1 w-full max-w-7xl mx-auto p-4 md:p-8 flex flex-col">
+          {children}
+        </div>
       </body>
     </html>
   );
