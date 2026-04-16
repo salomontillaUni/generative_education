a660784a81e6ff0309f2a377a66fccd427ff319e
Salomon Montilla <thebilystaxx@gmail.com>
2026-04-16 14:04:27 +0000

    corrige origenes permitidos

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        'https://solid-parakeet-695xjwv46w7vc9g9-3000.app.github.dev/', // Your specific codespace URL
+        'localhost:3000'
+      ],
+    },
+  },
 };
 
 export default nextConfig;
